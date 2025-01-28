545e196c02aff7917bb9180f0f66fdaa710f313a
Parth <limbachiyaparthh@gmail.com>
2025-01-28 18:17:09 +0530

    change in ui

--- academix/src/App.jsx
+++ academix/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import { Navbar } from './assets/Components/navbar/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      hello world
+      <Navbar/>
     </>
   )
 }
